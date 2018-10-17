64797186e003aa10901441e0319d1526c98f657e
Lauren Piontek <piontekle@gmail.com>
2018-10-17 18:01:43 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -3,5 +3,30 @@
 $(document).ready(function(){
 
 // call functions here
+function getIt() {
+$('p').on("click", function() {
+alert("Hey!");  
+});
+}
+
+function frameIt() {
+    $('img').on("load", function() {
+    img.class = tasty;
+  });
+}
+
+function pressIt() {
+  $(document).on("keydown", function() {
+    if (key.which == 71) {
+      alert("You da real oG.");
+    }
+  });
+}
+
+function submitIt() {
+  $('form').on("submit", function () {
+    alert("Your form is going to be submitted now.")
+  });
+}
 
 });
