c7317ae1f6214cf151e42334328dc466e0053dd5
Lauren Piontek <piontekle@gmail.com>
2018-10-17 18:09:16 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -17,7 +17,7 @@ function frameIt() {
 
 function pressIt() {
   $(document).on("keydown", function(key) {
-    if (key.which == 71) {
+    if (key.which === 71) {
       alert("You da real oG.");
     }
   });
